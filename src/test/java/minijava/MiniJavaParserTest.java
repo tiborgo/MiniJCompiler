004cc29012b1053ed60fefa9565a403d39b9c834
Michael Seifert <mseifert@error-reports.org>
2014-10-27 18:23:27 +0100

    Added a test that runs the parser on all examples that should cause a parse error.

--- src/test/java/minijava/MiniJavaParserTest.java
+++ src/test/java/minijava/MiniJavaParserTest.java
@@ -1,5 +1,7 @@
 package minijava;
 
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
@@ -13,11 +15,13 @@ import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.junit.Test;
 
 public class MiniJavaParserTest {
 	private static final Path EXAMPLE_PROGRAM_PATH_BASE = Paths.get("src/test/resources/minijava-examples");
 	private static final Path EXAMPLE_PROGRAM_PATH_WORKING = EXAMPLE_PROGRAM_PATH_BASE.resolve("working");
+	private static final Path EXAMPLE_PROGRAM_PATH_FAILING = EXAMPLE_PROGRAM_PATH_BASE.resolve("shouldFail/ParseErrors");
 
 	@Test
 	public void testParseWorkingExamples() throws IOException {
@@ -34,5 +38,31 @@ public class MiniJavaParserTest {
 			}
 		};
 		Files.walkFileTree(EXAMPLE_PROGRAM_PATH_WORKING, workingFilesVisitior);
+	}
+
+	@Test
+	public void testParseFailingExamples() throws IOException {
+		FileVisitor<Path> workingFilesVisitior = new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				ANTLRFileStream antlrStream = new ANTLRFileStream(file.toString());
+				MiniJavaLexer lexer = new MiniJavaLexer(antlrStream);
+				TokenStream tokens = new CommonTokenStream(lexer);
+				MiniJavaParser parser = new MiniJavaParser(tokens);
+				parser.setErrorHandler(new BailErrorStrategy());
+				/*
+				 * Catch all exceptions first, to ensure that every single
+				 * compilation unit exits with an Exception. Otherwise, this
+				 * method will return after the first piece of code.
+				 */
+				try {
+					parser.prog();
+					fail("The example "+file.toString()+" should have failed, but was accepted by the parser.");
+				} catch (ParseCancellationException e) {
+				}
+				return super.visitFile(file, attrs);
+			}
+		};
+		Files.walkFileTree(EXAMPLE_PROGRAM_PATH_FAILING, workingFilesVisitior);
 	}
 }
