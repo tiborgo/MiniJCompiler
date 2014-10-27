368def8b4d7e09a897c29affa1f367e129bcb37e
Michael Seifert <mseifert@error-reports.org>
2014-10-27 17:43:46 +0100

    Added first draft for SymbolTableVisitor.

---
+++ src/main/java/minijava/SymbolTable.java
@@ -0,0 +1,12 @@
+package minijava;
+
+import java.util.Map;
+
+public class SymbolTable {
+	private SymbolTable outerEnvironment;
+	private Map<String, String> innerEnvironment;
+
+	/*public addType(String type) {
+		innerEnvironment
+	}*/
+}
---
+++ src/main/java/minijava/SymbolTableVisitor.java
@@ -0,0 +1,65 @@
+package minijava;
+
+import minijava.MiniJavaParser.ClassBodyContext;
+import minijava.MiniJavaParser.ClassDeclarationExtendsContext;
+import minijava.MiniJavaParser.ClassDeclarationSimpleContext;
+import minijava.MiniJavaParser.MethodDeclarationContext;
+import minijava.MiniJavaParser.OtherTypeContext;
+import minijava.MiniJavaParser.VarDeclarationContext;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class SymbolTableVisitor extends MiniJavaBaseVisitor<SymbolTable> {
+	private final Program program;
+	private Class currentClass;
+
+	public SymbolTableVisitor() {
+		program = new Program();
+		Class intClass = new Class("int");
+		program.add(intClass);
+		Class intArrayClass = new Class("int[]");
+		program.add(intArrayClass);
+		Class booleanClass = new Class("boolean");
+		program.add(booleanClass);
+	}
+
+	@Override
+	public SymbolTable visitVarDeclaration(VarDeclarationContext ctx) {
+
+		return super.visitVarDeclaration(ctx);
+	}
+
+	@Override
+	public SymbolTable visitClassDeclarationExtends(ClassDeclarationExtendsContext ctx) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public SymbolTable visitClassDeclarationSimple(ClassDeclarationSimpleContext ctx) {
+		String className = ctx.identifier().IDENTIFIER().getText();
+		if (program.contains(className)) {
+			System.out.println("Class "+className+" has already been declared.");
+		} else {
+			Class clazz = new Class(className);
+			currentClass = clazz;
+			program.add(clazz);
+		}
+		return visit(ctx.classBody());
+	}
+
+	@Override
+	public SymbolTable visitClassBody(ClassBodyContext ctx) {
+		for (VarDeclarationContext varDeclaration : ctx.varDeclaration()) {
+			if (varDeclaration.type() instanceof OtherTypeContext) {
+				OtherTypeContext typeContext = (OtherTypeContext) varDeclaration.type();
+				String typeName = typeContext.identifier().IDENTIFIER().getText();
+				String variableName = varDeclaration.identifier().IDENTIFIER().getText();
+				currentClass.fields.put(variableName, typeName);
+			}
+		}
+
+		for (MethodDeclarationContext methodDeclaration : ctx.methodDeclaration()) {
+			visit(methodDeclaration);
+		}
+		return super.visitClassBody(ctx);
+	}
+}
