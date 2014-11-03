3a206bf7534689e437b4d80855cb01e5a583e5ef
Michael Seifert <mseifert@error-reports.org>
2014-11-03 18:08:46 +0100

    Implemented some of the methods that create the intermediate language from the AST.

--- src/main/java/minijava/intermediate/IntermediateVisitor.java
+++ src/main/java/minijava/intermediate/IntermediateVisitor.java
@@ -1,13 +1,22 @@
 package minijava.intermediate;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import minijava.intermediate.tree.TreeExp;
+import minijava.intermediate.tree.TreeExpCALL;
+import minijava.intermediate.tree.TreeExpCONST;
+import minijava.intermediate.tree.TreeExpMEM;
+import minijava.intermediate.tree.TreeExpNAME;
+import minijava.intermediate.tree.TreeExpOP;
+import minijava.intermediate.tree.TreeExpOP.Op;
 import minijava.intermediate.tree.TreeStm;
 import minijava.syntax.ast.DeclClass;
 import minijava.syntax.ast.DeclMain;
 import minijava.syntax.ast.DeclMeth;
 import minijava.syntax.ast.DeclVar;
+import minijava.syntax.ast.Exp;
 import minijava.syntax.ast.ExpArrayGet;
 import minijava.syntax.ast.ExpArrayLength;
 import minijava.syntax.ast.ExpBinOp;
@@ -92,14 +101,12 @@ public class IntermediateVisitor {
 
 	@Override
 	public TreeExp visit(ExpTrue e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		return new TreeExpCONST(1);
 	}
 
 	@Override
 	public TreeExp visit(ExpFalse e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		return new TreeExpCONST(0);
 	}
 
 	@Override
@@ -110,8 +117,8 @@ public class IntermediateVisitor {
 
 	@Override
 	public TreeExp visit(ExpNewIntArray e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		TreeExp arraySize = e.size.accept(this);
+		return new TreeExpCALL(new TreeExpNAME(new Label("L_halloc")), Collections.singletonList(arraySize));
 	}
 
 	@Override
@@ -122,19 +129,49 @@ public class IntermediateVisitor {
 
 	@Override
 	public TreeExp visit(ExpNeg e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		TreeExp negatedExpression = e.body.accept(this);
+		if (!(negatedExpression instanceof TreeExpCONST)) {
+			throw new IllegalArgumentException("Unable to negate the expression \""+e.prettyPrint()+"\"");
+		}
+		TreeExpCONST negatedBoolean = (TreeExpCONST) negatedExpression;
+		assert(negatedBoolean.value == 0 || negatedBoolean.value == 1);
+		return new TreeExpCONST(1 - negatedBoolean.value);
 	}
 
 	@Override
 	public TreeExp visit(ExpBinOp e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		Op operator = null;
+		switch (e.op) {
+		case PLUS:
+			operator = Op.PLUS;
+			break;
+		case MINUS:
+			operator = Op.MINUS;
+			break;
+		case TIMES:
+			operator = Op.MUL;
+			break;
+		case DIV:
+			operator = Op.DIV;
+			break;
+		case AND:
+			operator = Op.AND;
+			break;
+		case LT:
+			// TODO
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown operator: "+e.op);
+		}
+		return new TreeExpOP(operator, e.left.accept(this), e.right.accept(this));
 	}
 
 	@Override
 	public TreeExp visit(ExpArrayGet e) throws RuntimeException {
-		// TODO Auto-generated method stub
+		// MEM(BinOp(+, translate(array), BinOp(*, translate(index), BinOp(+, WORD_SIZE, CONST(1))))
+		/*TreeExp array = e.array.accept(this);
+		TreeExp index = e.index.accept(this);
+		new TreeExpMEM(addr);*/
 		return null;
 	}
 
@@ -146,23 +183,36 @@ public class IntermediateVisitor {
 
 	@Override
 	public TreeExp visit(ExpInvoke e) throws RuntimeException {
-		// TODO Auto-generated method stub
+		// TODO
+		/*TreeExp object = e.obj.accept(this);
+		if (!(object instanceof TreeExpCONST)) {
+			throw new RuntimeException("Unable to invoke method on object \""+e.obj.prettyPrint()+"\"");
+		}*/
+		String className = "";
+		String methodName = e.method;
+		// FIXME: Retrieve function label with the respective mangled name
+		TreeExp function = new TreeExpNAME(new Label(mangle(className, methodName)));
+		List<TreeExp> arguments = new ArrayList<>(e.args.size());
+		for (Exp exp : e.args) {
+			TreeExp iRExpression = exp.accept(this);
+			arguments.add(iRExpression);
+		}
 		return null;
 	}
 
 	@Override
 	public TreeExp visit(ExpIntConst e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		return new TreeExpCONST(e.value);
 	}
 
 	@Override
 	public TreeExp visit(ExpId e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		return new TreeExpNAME(new Label(e.id));
 	}
 
-    
+	private String mangle(String className, String methodName) {
+		return className+"$"+methodName;
+	}
   }
 
   static class IntermediateVisitorStm implements StmVisitor<TreeStm, RuntimeException> {
@@ -209,6 +259,6 @@ public class IntermediateVisitor {
 		return null;
 	}
 
-    
+
   }
 }
