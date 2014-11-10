1417b25d8874daa930c543000b44bf407d4e8dba
Michael Seifert <mseifert@error-reports.org>
2014-11-10 11:52:47 +0100

    When retrieving the Variable object of an identity expression, TypeCheckVisitor now also considers class fields.

--- src/main/java/minijava/ast/visitors/TypeCheckVisitor.java
+++ src/main/java/minijava/ast/visitors/TypeCheckVisitor.java
@@ -278,8 +278,12 @@ public class TypeCheckVisitor implements PrgVisitor<Boolean, RuntimeException>,
 
 	@Override
 	public Ty visit(ExpId e) throws RuntimeException {
+		// TODO: Should be replaced with a lookup in an appropriate data structure that honours variable visibility
 		Variable object = methodContext.get(e.id);
-		
+		if (object == null) {
+			object = classContext.fields.get(e.id);
+		}
+
 		if (object == null) {
 			// TODO: error
 			System.err.println("Unknown variable \""+e.id+"\"");
