3d34c9a078b6a5bd08b849e434e711c676df26f6
Michael Seifert <mseifert@error-reports.org>
2014-11-10 14:46:26 +0100

    Added an interface that represents a compiler frontend to a language. MiniJavaCompiler implements Frontend.

--- src/main/java/minijava/MiniJavaCompiler.java
+++ src/main/java/minijava/MiniJavaCompiler.java
@@ -1,39 +1,39 @@
 package minijava;
 import java.io.IOException;
 
-import minijava.MiniJavaLexer;
-import minijava.MiniJavaParser;
 import minijava.antlr.visitors.ASTVisitor;
 import minijava.ast.rules.Prg;
 import minijava.ast.visitors.PrettyPrintVisitor;
 import minijava.ast.visitors.SymbolTableVisitor;
 import minijava.ast.visitors.TypeCheckVisitor;
 import minijava.symboltable.tree.Program;
-
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-//import MiniJavaPrettyPrintVisitor;
+public class MiniJavaCompiler implements Frontend {
 
-public class MiniJavaCompiler {
+	@Override
+	public Prg getAbstractSyntaxTree(String filePath) throws IOException {
+		ANTLRFileStream reader = new ANTLRFileStream(filePath);
+		MiniJavaLexer lexer = new MiniJavaLexer((CharStream) reader);
+		TokenStream tokens = new CommonTokenStream(lexer);
+		MiniJavaParser parser = new MiniJavaParser(tokens);
+		ParseTree parseTree = parser.prog();
+		ASTVisitor astVisitor = new ASTVisitor();
+		Prg program = (Prg) astVisitor.visit(parseTree);
+		return program;
+	}
 
 	public static void main(String[] args) {
 		// TODO code application logic here
 		// SymbolTable table = new SymbolTable();
 
+		MiniJavaCompiler compiler = new MiniJavaCompiler();
 		try {
-			ANTLRFileStream reader = new ANTLRFileStream(
-					"src/test/resources/minijava-examples/working/ArrSum.java");
-			MiniJavaLexer lexer = new MiniJavaLexer((CharStream) reader);
-			TokenStream tokens = new CommonTokenStream(lexer);
-			MiniJavaParser parser = new MiniJavaParser(tokens);
-			ParseTree tree = parser.prog();
-			ASTVisitor astVisitor = new ASTVisitor();
-			Prg program = (Prg) astVisitor.visit(tree);
-
+			Prg program = compiler.getAbstractSyntaxTree("src/test/resources/minijava-examples/working/ArrSum.java");
 			PrettyPrintVisitor prettyPrintVisitor = new PrettyPrintVisitor("");
 			String output = program.accept(prettyPrintVisitor);
 			System.out.print(output);
