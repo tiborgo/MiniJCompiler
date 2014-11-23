856e77cd2eb8550814c900481e37eb9c0d3a219e
Michael Seifert <mseifert@error-reports.org>
2014-11-23 20:40:06 +0100

    Added a test case for the base block Generator.

---
+++ src/test/java/minijava/ast/visitors/baseblocks/GeneratorTest.java
@@ -0,0 +1,29 @@
+package minijava.ast.visitors.baseblocks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+import minijava.intermediate.Label;
+import minijava.intermediate.tree.TreeExpNAME;
+import minijava.intermediate.tree.TreeStm;
+import minijava.intermediate.tree.TreeStmJUMP;
+import minijava.intermediate.tree.TreeStmLABEL;
+import org.junit.Test;
+
+public class GeneratorTest {
+
+	@Test
+	public void testGenerateLabelWithinBlock() throws Exception {
+		List<TreeStm> statements = new ArrayList<>();
+		// Method start
+		Label methodLabel = new Label();
+		statements.add(new TreeStmLABEL(methodLabel));
+		// Jump to start
+		statements.add(new TreeStmJUMP(new TreeExpNAME(methodLabel), Collections.singletonList(methodLabel)));
+		Generator.BaseBlockContainer baseBlockContainer = Generator.generate(statements, new Label());
+		assertEquals(1, baseBlockContainer.baseBlocks.size());
+	}
+}
