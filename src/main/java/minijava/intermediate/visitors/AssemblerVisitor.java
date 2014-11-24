e0c170fb8a076f8cc987b269fbd1ffa29259ea4e
Michael Seifert <mseifert@error-reports.org>
2014-11-24 17:39:16 +0100

    First draft of the translation of expressions into assembler instructions in AssemblerVisitor.

--- src/main/java/minijava/intermediate/visitors/AssemblerVisitor.java
+++ src/main/java/minijava/intermediate/visitors/AssemblerVisitor.java
@@ -3,6 +3,7 @@ package minijava.intermediate.visitors;
 import java.util.List;
 
 import minijava.backend.Assem;
+import minijava.backend.i386.AssemBinaryOp;
 import minijava.backend.i386.Operand;
 import minijava.intermediate.FragmentProc;
 import minijava.intermediate.FragmentVisitor;
@@ -23,10 +24,10 @@ import minijava.intermediate.tree.TreeStmSEQ;
 
 public class AssemblerVisitor implements
 	FragmentVisitor<List<TreeStm>, FragmentProc<List<Assem>>>,
-	TreeStmVisitor<List<Assem>, RuntimeException>, 
+	TreeStmVisitor<List<Assem>, RuntimeException>,
  	TreeExpVisitor<Operand, RuntimeException> {
 
-	
+
 	@Override
 	public FragmentProc<List<Assem>> visit(FragmentProc<List<TreeStm>> fragProc) {
 		// TODO Auto-generated method stub
@@ -41,32 +42,66 @@ public class AssemblerVisitor implements
 
 	@Override
 	public Operand visit(TreeExpCONST e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		return new Operand.Imm(e.value) ;
 	}
 
 	@Override
 	public Operand visit(TreeExpESEQ e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new RuntimeException("There must not be any ESEQ statements when " +
+				"translating the intermediate representation into assembly.");
 	}
 
 	@Override
 	public Operand visit(TreeExpMEM e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		return e.addr.accept(this);
 	}
 
 	@Override
 	public Operand visit(TreeExpNAME e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		return new Operand.Label(e.label);
 	}
 
 	@Override
 	public Operand visit(TreeExpOP e) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		Operand o1 = e.left.accept(this);
+		Operand o2 = e.right.accept(this);
+		AssemBinaryOp.Kind operator = null;
+		switch (e.op) {
+			case PLUS:
+				operator = AssemBinaryOp.Kind.ADD;
+				break;
+			case MINUS:
+				operator = AssemBinaryOp.Kind.SUB;
+				break;
+			case MUL:
+				// TODO: Multiplication uses the unary operation imul
+			case DIV:
+				// TODO: Multiplication uses the unary operation idiv
+			case AND:
+				operator = AssemBinaryOp.Kind.AND;
+				break;
+			case OR:
+				operator = AssemBinaryOp.Kind.OR;
+				break;
+			case LSHIFT:
+				operator = AssemBinaryOp.Kind.SHL;
+				break;
+			case RSHIFT:
+				operator = AssemBinaryOp.Kind.SHR;
+				break;
+			case ARSHIFT:
+				operator = AssemBinaryOp.Kind.SAR;
+				break;
+			case XOR:
+				operator = AssemBinaryOp.Kind.XOR;
+				break;
+			default:
+				throw new UnsupportedOperationException(
+						"Unsupported operator \""+ e.op + "\"");
+		}
+		AssemBinaryOp binaryOperation = new AssemBinaryOp(operator, o1, o2);
+		// TODO: Add binary operation to the list of assembler instructions
+		return o1;
 	}
 
 	@Override
