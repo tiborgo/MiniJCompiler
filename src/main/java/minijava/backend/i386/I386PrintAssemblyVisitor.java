f5cf7ab36c422afded47fbfee32269e54e6d08f1
Michael Seifert <mseifert@error-reports.org>
2014-12-08 17:42:55 +0100

    Fixed some of the pretty print code. GCC accepts assembly on stdin.

--- src/main/java/minijava/backend/i386/I386PrintAssemblyVisitor.java
+++ src/main/java/minijava/backend/i386/I386PrintAssemblyVisitor.java
@@ -7,32 +7,32 @@ public class I386PrintAssemblyVisitor implements
 
 	@Override
 	public String visit(AssemBinaryOp assem) {
-		
+
 		StringBuilder operation = new StringBuilder()
 			.append(assem.kind.name())
 			.append(" ")
 			.append(assem.dst.accept(this))
 			.append(",")
 			.append(assem.src.accept(this));
-		
+
 		return operation.toString();
 	}
-	
+
 	@Override
 	public String visit(AssemInstr assem) {
 		return assem.kind.name();
 	}
-	
+
 	@Override
 	public String visit(AssemJump assem) {
-		
+
 		StringBuilder operation = new StringBuilder()
 			.append(assem.kind.name());
-		
+
 		if (assem.kind == AssemJump.Kind.J) {
 			operation.append(assem.cond.name());
 		}
-		
+
 		if (assem.dest instanceof Operand.Label) {
 			operation.append(" ")
 				.append(assem.dest.accept(this));
@@ -40,66 +40,69 @@ public class I386PrintAssemblyVisitor implements
 		else {
 			throw new UnsupportedOperationException("Can only print jumps to labels");
 		}
-		
+
 		return operation.toString();
 	}
-	
+
 	@Override
 	public String visit(AssemUnaryOp assem) {
 		StringBuilder operation = new StringBuilder()
 			.append(assem.kind.name())
 			.append(" ")
 			.append(assem.op.accept(this));
-		
+
 		return operation.toString();
 	}
-	
+
 	@Override
 	public String visit(AssemLabel assem) {
-		return assem.label.toString();
+		return assem.label.toString() + ":";
 	}
-	
+
 	//-----
-	
+
 	public String visit(Operand.Imm op) {
 		return Integer.toString(op.imm);
 	}
-	
+
 	public String visit(Operand.Reg op) {
 		return "%" + op.reg.toString();
 	}
-	
+
 	public String visit(Operand.Mem op) {
 		// base + index * size + displacement
 		StringBuilder operation = new StringBuilder();
-		
+		operation.append("[");
+
 		if (op.base != null) {
+			operation.append("%");
 			operation.append(op.base.toString());
-			
+
 		}
-		
+
 		if (op.index != null) {
 			operation
 				.append("+")
 				.append(op.index);
 		}
-		
+
 		if (op.scale != null) {
 			operation
 				.append("*")
 				.append(op.scale);
 		}
-		
+
 		if (op.displacement != 0) {
 			operation
 				.append("+")
 				.append(op.displacement);
 		}
-			
-		
+		operation.append("]");
+
+
 		return operation.toString();
 	}
-	
+
 	public String visit(Operand.Label op) {
 		return op.label.toString();
 	}
