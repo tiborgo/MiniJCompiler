a1434b6fbd7007b34dbefdebe6a770b928e6fb37
Tibor Goldschwendt <goldschwendt@cip.ifi.lmu.de>
2014-12-15 17:22:35 +0100

    Added interface for control flow analysis.

--- src/main/java/minijava/util/SimpleGraph.java
+++ src/main/java/minijava/util/SimpleGraph.java
@@ -1,14 +1,9 @@
 package minijava.util;
 
 import java.io.PrintStream;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
