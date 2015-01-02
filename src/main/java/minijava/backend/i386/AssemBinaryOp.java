ffeefeb3c158f421d2b9b527e5c32b9468ff9dca
Michael Seifert <mseifert@error-reports.org>
2015-01-02 19:33:56 +0100

    AssemBinaryOp extends Instruction.

--- src/main/java/minijava/backend/i386/AssemBinaryOp.java
+++ src/main/java/minijava/backend/i386/AssemBinaryOp.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import minijava.backend.Assem;
 import minijava.backend.AssemVisitor;
-import minijava.intermediate.Label;
+import minijava.backend.DefaultInstruction;
 import minijava.intermediate.Temp;
 import minijava.util.Function;
 import minijava.util.Pair;
 
-public final class AssemBinaryOp implements Assem {
+public final class AssemBinaryOp extends DefaultInstruction {
 
 	public static enum Kind {
 
@@ -22,6 +22,7 @@ public final class AssemBinaryOp implements Assem {
 	public final Kind kind;
 
 	public AssemBinaryOp(Kind kind, Operand dst, Operand src) {
+		super(dst, src);
 		assert (kind != null && src != null && dst != null);
 		assert (!((src instanceof Operand.Mem) && (dst instanceof Operand.Mem)));
 		assert (kind != Kind.LEA || ((src instanceof Operand.Mem) && (dst instanceof Operand.Reg)));
@@ -30,32 +31,16 @@ public final class AssemBinaryOp implements Assem {
 		this.kind = kind;
 	}
 
-	public List<Temp> use() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
 	public List<Temp> def() {
 		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	public List<Label> jumps() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	public boolean isFallThrough() {
-		return true;
 	}
 
 	public Pair<Temp, Temp> isMoveBetweenTemps() {
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
-	public Label isLabel() {
-		return null;
-	}
-
 	public String toString() {
-		throw new UnsupportedOperationException("Not supported yet.");
+		return kind.toString();
 	}
 
 	public Assem rename(Function<Temp, Temp> sigma) {
@@ -63,8 +48,7 @@ public final class AssemBinaryOp implements Assem {
 	}
 
 	@Override
-	public <A, T extends Throwable> A accept(AssemVisitor<A, T> visitor)
-			throws T {
+	public <A, T extends Throwable> A accept(AssemVisitor<A, T> visitor) throws T {
 		return visitor.visit(this);
 	}
 }
