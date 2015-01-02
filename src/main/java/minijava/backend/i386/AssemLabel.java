ee5c133976de15e33120c312df547b90ee1c10b5
Michael Seifert <mseifert@error-reports.org>
2015-01-02 14:09:26 +0100

    AssemLabel extends Directive.

--- src/main/java/minijava/backend/i386/AssemLabel.java
+++ src/main/java/minijava/backend/i386/AssemLabel.java
@@ -1,58 +1,27 @@
 package minijava.backend.i386;
 
-import java.util.Collections;
-import java.util.List;
-
-import minijava.backend.Assem;
 import minijava.backend.AssemVisitor;
 import minijava.intermediate.Label;
-import minijava.intermediate.Temp;
-import minijava.util.Function;
-import minijava.util.Pair;
 
-public final class AssemLabel implements Assem {
-
+public final class AssemLabel extends Directive {
 	public final Label label;
 
 	public AssemLabel(Label label) {
 		this.label = label;
 	}
 
-	public List<Temp> use() {
-		return Collections.emptyList();
-	}
-
-	public List<Temp> def() {
-		return Collections.emptyList();
-	}
-
-	public List<Label> jumps() {
-		return Collections.emptyList();
-	}
-
-	public boolean isFallThrough() {
-		return true;
-	}
-
-	public Pair<Temp, Temp> isMoveBetweenTemps() {
-		return null;
-	}
-
+	@Override
 	public Label isLabel() {
 		return label;
 	}
 
+	@Override
 	public String toString() {
 		return label + ":\n";
 	}
 
-	public Assem rename(Function<Temp, Temp> sigma) {
-		return this;
-	}
-
 	@Override
-	public <A, T extends Throwable> A accept(AssemVisitor<A, T> visitor)
-			throws T {
+	public <A, T extends Throwable> A accept(AssemVisitor<A, T> visitor) throws T {
 		return visitor.visit(this);
 	}
 }
