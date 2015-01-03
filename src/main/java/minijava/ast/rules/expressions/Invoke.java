e31983108e9cad64b760fd6813c10bd4d5832f7b
Michael Seifert <mseifert@error-reports.org>
2015-01-03 14:37:04 +0100

    Reformatted Invoke expression.

--- src/main/java/minijava/ast/rules/expressions/Invoke.java
+++ src/main/java/minijava/ast/rules/expressions/Invoke.java
@@ -3,19 +3,18 @@ package minijava.ast.rules.expressions;
 import java.util.List;
 
 public class Invoke extends Expression {
+	public final Expression obj;
+	public final String method;
+	public final List<Expression> args;
 
-  final public Expression obj;
-  final public String method;
-  final public List<Expression> args;
+	public Invoke(Expression obj, String method, List<Expression> args) {
+		this.obj = obj;
+		this.method = method;
+		this.args = args;
+	}
 
-  public Invoke(Expression obj, String method, List<Expression> args) {
-    this.obj = obj;
-    this.method = method;
-    this.args = args;
-  }
-
-  @Override
-  public <A, T extends Throwable> A accept(ExpressionVisitor<A, T> v) throws T{
-    return v.visit(this);
-  }
+	@Override
+	public <A, T extends Throwable> A accept(ExpressionVisitor<A, T> v) throws T {
+		return v.visit(this);
+	}
 }
