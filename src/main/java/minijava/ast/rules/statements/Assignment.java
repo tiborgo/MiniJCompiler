5d9eab56bbd9a5692429b1d052353b616aa4ceeb
Michael Seifert <mseifert@error-reports.org>
2015-01-03 21:44:42 +0100

    Reformatted Assignment.

--- src/main/java/minijava/ast/rules/statements/Assignment.java
+++ src/main/java/minijava/ast/rules/statements/Assignment.java
@@ -4,16 +4,16 @@ import minijava.ast.rules.expressions.Expression;
 
 public class Assignment extends Statement {
 
-  final public String id;
-  final public Expression rhs;
+	public final String id;
+	public final Expression rhs;
 
-  public Assignment(String id, Expression rhs) {
-    this.id = id;
-    this.rhs = rhs;
-  }
+	public Assignment(String id, Expression rhs) {
+		this.id = id;
+		this.rhs = rhs;
+	}
 
-  @Override
-  public <A, T extends Throwable> A accept(StatementVisitor<A, T> v) throws T {
-    return v.visit(this);
-  }
+	@Override
+	public <A, T extends Throwable> A accept(StatementVisitor<A, T> v) throws T {
+		return v.visit(this);
+	}
 }
