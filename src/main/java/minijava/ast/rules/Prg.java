443e4f67cf141d1a7ca9d921931087cea5d728b9
Michael Seifert <mseifert@error-reports.org>
2015-01-03 16:18:19 +0100

    Reformatted Prg.

--- src/main/java/minijava/ast/rules/Prg.java
+++ src/main/java/minijava/ast/rules/Prg.java
@@ -6,16 +6,15 @@ import minijava.ast.rules.declarations.Class;
 import minijava.ast.rules.declarations.Main;
 
 public class Prg {
+	public final Main mainClass;
+	public final List<Class> classes;
 
-  final public Main mainClass;
-  final public List<Class> classes;
+	public Prg(Main mainClass, List<Class> classes) {
+		this.mainClass = mainClass;
+		this.classes = classes;
+	}
 
-  public Prg(Main mainClass, List<Class> classes) {
-    this.mainClass = mainClass;
-    this.classes = classes;
-  }
-
-  public <A, T extends Throwable> A accept(PrgVisitor<A, T> v) throws T {
-    return v.visit(this);
-  }
+	public <A, T extends Throwable> A accept(PrgVisitor<A, T> v) throws T {
+		return v.visit(this);
+	}
 }
