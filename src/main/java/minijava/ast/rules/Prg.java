3c5db592823ec55fc0f93ad2f07916783a442cc8
Michael Seifert <mseifert@error-reports.org>
2015-01-03 16:31:14 +0100

    Prg.getClasses now returns a Collection. Prg stores the classes by name in a map.

--- src/main/java/minijava/ast/rules/Prg.java
+++ src/main/java/minijava/ast/rules/Prg.java
@@ -1,25 +1,31 @@
 package minijava.ast.rules;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import minijava.ast.rules.declarations.Class;
 import minijava.ast.rules.declarations.Main;
 
 public class Prg {
 	public final Main mainClass;
-	private final List<Class> classes;
+	private final Map<String, Class> classes;
 
 	public Prg(Main mainClass, List<Class> classes) {
 		this.mainClass = mainClass;
-		this.classes = classes;
+		this.classes = new HashMap<>(classes.size());
+		for (Class clazz : classes) {
+			this.classes.put(clazz.className, clazz);
+		}
 	}
 
 	public <A, T extends Throwable> A accept(PrgVisitor<A, T> v) throws T {
 		return v.visit(this);
 	}
 
-	public List<Class> getClasses() {
-		return Collections.unmodifiableList(classes);
+	public Collection<Class> getClasses() {
+		return Collections.unmodifiableCollection(classes.values());
 	}
 }
