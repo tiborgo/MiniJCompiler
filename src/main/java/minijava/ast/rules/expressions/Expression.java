165e44759f876a9ed0554bf1d7a34e74e9849a36
Michael Seifert <mseifert@error-reports.org>
2015-01-03 14:37:49 +0100

    Expression stores a mutable Type member.

--- src/main/java/minijava/ast/rules/expressions/Expression.java
+++ src/main/java/minijava/ast/rules/expressions/Expression.java
@@ -1,6 +1,9 @@
 package minijava.ast.rules.expressions;
 
+import minijava.ast.rules.types.Type;
+
 public abstract class Expression {
+  public Type type;
 
   public abstract <A, T extends Throwable> A accept(ExpressionVisitor<A, T> v) throws T;
 }
