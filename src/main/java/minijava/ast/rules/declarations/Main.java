2d4eed95d42bcac508608fe0433f067819671857
Michael Seifert <mseifert@error-reports.org>
2015-01-03 19:38:07 +0100

    Fixed a bug that caused the implicit return value of the main method to be missing.

--- src/main/java/minijava/ast/rules/declarations/Main.java
+++ src/main/java/minijava/ast/rules/declarations/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import minijava.ast.rules.Parameter;
+import minijava.ast.rules.expressions.IntConstant;
 import minijava.ast.rules.statements.Statement;
 
 public class Main extends Class {
@@ -17,7 +18,7 @@ public class Main extends Class {
 				Collections.<Variable>emptyList(),
 				// FIXME: Main method has argument of type String[]
 				Arrays.asList(new Method(new minijava.ast.rules.types.Void(), "main",
-					Arrays.asList(new Parameter(mainArg, null)), Collections.<Variable>emptyList(), mainBody, null)
+					Arrays.asList(new Parameter(mainArg, null)), Collections.<Variable>emptyList(), mainBody, new IntConstant(0))
 				)
 		);
 		this.mainMethod = methods.get(0);
