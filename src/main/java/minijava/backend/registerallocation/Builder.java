2e21766f8b31279ac173a78793b3c1e3734b6071
Tibor Goldschwendt <goldschwendt@cip.ifi.lmu.de>
2015-01-03 18:25:57 +0100

    Added edges to pre-colored graph

--- src/main/java/minijava/backend/registerallocation/Builder.java
+++ src/main/java/minijava/backend/registerallocation/Builder.java
@@ -1,7 +1,9 @@
 package minijava.backend.registerallocation;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import minijava.backend.MachineSpecifics;
 import minijava.intermediate.Temp;
@@ -13,6 +15,7 @@ public class Builder {
 		
 		SimpleGraph<ColoredNode> coloredInterferenceGraph = new SimpleGraph<>(interferenceGraph.getName());
 		List<Temp> colors = Arrays.asList(machineSpecifics.getGeneralPurposeRegisters());
+		Map<Temp, SimpleGraph<ColoredNode>.Node> nodes = new HashMap<>();
 
 		for (SimpleGraph<Temp>.Node tNode : interferenceGraph.nodeSet()) {
 			Temp color = null;
@@ -20,7 +23,17 @@ public class Builder {
 				color = tNode.info;
 			}
 			
-			coloredInterferenceGraph.new Node(new ColoredNode(tNode.info, color));
+			SimpleGraph<ColoredNode>.Node node = coloredInterferenceGraph.new Node(new ColoredNode(tNode.info, color));
+			nodes.put(tNode.info, node);
+		}
+		
+		for (SimpleGraph<Temp>.Node tNode : interferenceGraph.nodeSet()) {
+			for (SimpleGraph<Temp>.Node sNode : tNode.successors()) {
+				coloredInterferenceGraph.addEdge(nodes.get(tNode.info), nodes.get(sNode.info));
+			}
+			for (SimpleGraph<Temp>.Node pNode : tNode.predecessors()) {
+				coloredInterferenceGraph.addEdge(nodes.get(pNode.info), nodes.get(tNode.info));
+			}
 		}
 		
 		return coloredInterferenceGraph;
