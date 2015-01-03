00d4b2a7da72a2abef8defd9be6298ef723bd880
Michael Seifert <mseifert@error-reports.org>
2015-01-03 19:32:34 +0100

    Main inherits from Class.

--- src/main/java/minijava/ast/rules/declarations/Main.java
+++ src/main/java/minijava/ast/rules/declarations/Main.java
@@ -6,16 +6,21 @@ import java.util.Collections;
 import minijava.ast.rules.Parameter;
 import minijava.ast.rules.statements.Statement;
 
-public class Main extends Declaration {
+public class Main extends Class {
 
-	public final String className;
 	public final Method mainMethod;
 
+	// TODO: Main method should be passed in constructor
 	public Main(String className, String mainArg, Statement mainBody) {
-		this.className = className;
-		// FIXME: Main method has argument of type String[]
-		this.mainMethod = new Method(new minijava.ast.rules.types.Void(), "main",
-				Arrays.asList(new Parameter(mainArg, null)), Collections.<Variable>emptyList(), mainBody, null);
+		super(className,
+				null,
+				Collections.<Variable>emptyList(),
+				// FIXME: Main method has argument of type String[]
+				Arrays.asList(new Method(new minijava.ast.rules.types.Void(), "main",
+					Arrays.asList(new Parameter(mainArg, null)), Collections.<Variable>emptyList(), mainBody, null)
+				)
+		);
+		this.mainMethod = methods.get(0);
 	}
 
 	@Override
