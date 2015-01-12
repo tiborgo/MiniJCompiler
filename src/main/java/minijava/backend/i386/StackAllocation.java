169b0ea3e7f5d6cc7e5dcd964cf604d5d2c8703c
Michael Seifert <mseifert@error-reports.org>
2015-01-12 17:42:02 +0100

    AssemBinaryOp is no longer final. Added a separate instruction for the allocation of the stack size in the fragment prologue.

---
+++ src/main/java/minijava/backend/i386/StackAllocation.java
@@ -0,0 +1,11 @@
+package minijava.backend.i386;
+
+public class StackAllocation extends AssemBinaryOp {
+	public StackAllocation(Operand.Imm byteCount) {
+		super(Kind.SUB, I386MachineSpecifics.ESP, byteCount);
+	}
+
+	public void setByteCount(Operand.Imm byteCount) {
+		this.dst = byteCount;
+	}
+}
