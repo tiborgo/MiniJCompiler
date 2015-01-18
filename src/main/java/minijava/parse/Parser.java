bb51b193ac1086d1b9e8c0f85898f91380738937
Tibor Goldschwendt <goldschwendt@cip.ifi.lmu.de>
2015-01-18 20:00:10 +0100

    - Compiler test tests all files - Run test integrated into compiler test

--- src/main/java/minijava/parse/Parser.java
+++ src/main/java/minijava/parse/Parser.java
@@ -1,6 +1,7 @@
 package minijava.parse;
 
 import java.io.IOException;
+import java.util.BitSet;
 
 import minijava.Configuration;
 import minijava.Logger;
@@ -10,21 +11,73 @@ import minijava.parse.rules.Program;
 import minijava.parse.visitors.ASTVisitor;
 import minijava.parse.visitors.PrettyPrintVisitor;
 
+import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Parser {
+	
+	private static class ParserErrorListener implements ANTLRErrorListener {
+		
+		private boolean errorOccurred = false;
+
+		@Override
+		public void syntaxError(Recognizer<?, ?> recognizer,
+				Object offendingSymbol, int line, int charPositionInLine,
+				String msg, RecognitionException e) {
+			errorOccurred = true;
+		}
+
+		@Override
+		public void reportAmbiguity(org.antlr.v4.runtime.Parser recognizer,
+				DFA dfa, int startIndex, int stopIndex, boolean exact,
+				BitSet ambigAlts, ATNConfigSet configs) {
+			errorOccurred = true;
+		}
+
+		@Override
+		public void reportAttemptingFullContext(
+				org.antlr.v4.runtime.Parser recognizer, DFA dfa,
+				int startIndex, int stopIndex, BitSet conflictingAlts,
+				ATNConfigSet configs) {
+			errorOccurred = true;
+		}
+
+		@Override
+		public void reportContextSensitivity(
+				org.antlr.v4.runtime.Parser recognizer, DFA dfa,
+				int startIndex, int stopIndex, int prediction,
+				ATNConfigSet configs) {
+			errorOccurred = true;
+		}
+		
+		public boolean didErrorOccurr() {
+			return errorOccurred;
+		}
+		
+	}
 	
 	public static Program parse(Configuration config) throws ParserException {
 		try {
 			ANTLRFileStream reader = new ANTLRFileStream(config.inputFile);
 			MiniJavaLexer lexer = new MiniJavaLexer((CharStream) reader);
 			TokenStream tokens = new CommonTokenStream(lexer);
+			
 			MiniJavaParser parser = new MiniJavaParser(tokens);
+			ParserErrorListener listener = new ParserErrorListener();
+			parser.addErrorListener(listener);
 			ParseTree parseTree = parser.prog();
+			if (listener.didErrorOccurr()) {
+				throw new ParserException("Could not parse file");
+			}
+			
 			ASTVisitor astVisitor = new ASTVisitor();
 			Program program = (Program) astVisitor.visit(parseTree);
 			
@@ -38,6 +91,12 @@ public class Parser {
 		}
 		catch (IOException e) {
 			throw new ParserException("Antlr could not open file " + config.inputFile, e);
+		}
+		catch (ParserException e) {
+			throw e;
+		}
+		catch (Exception e) {
+			throw new ParserException("Could not parse file", e);
 		}
 	}
 }
--- src/test/java/minijava/TestFiles.java
+++ src/test/java/minijava/TestFiles.java
@@ -6,6 +6,9 @@ import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 
+import minijava.parse.ParserException;
+import minijava.semanticanalysis.SemanticAnalyserException;
+
 import org.apache.commons.io.FileUtils;
 
 public class TestFiles {
@@ -16,23 +19,24 @@ public class TestFiles {
 	public static final Path EXAMPLE_PROGRAM_PATH_TYPE_FAILING = EXAMPLE_PROGRAM_PATH_BASE.resolve("typeErrors");
 	public static final Path EXAMPLE_PROGRAM_PATH_RUNTIME_FAILING = EXAMPLE_PROGRAM_PATH_BASE.resolve("runtimeErrors");
 	
-	private static List<Object[]> getFiles(boolean work, Path... paths) {
+	private static List<Object[]> getFiles(Class<? extends Exception> exceptionClass, Path... paths) {
 		List<Object[]> files = new LinkedList<>();
 		
 		for (Path path : paths) {
 			for (File file : FileUtils.listFiles(path.toFile(), null, true)) {
-				files.add(new Object[] { file, work });
+				files.add(new Object[] { file, exceptionClass });
 			}
 		}
 
 		return files;
 	}
 	
-	public static List<Object[]> getWorkingFiles(Path... paths) {
-		return getFiles(true, paths);
-	}
-	
-	public static List<Object[]> getFailingFiles(Path... paths) {
-		return getFiles(false, paths);
+	public static List<Object[]> getFiles() {
+		List<Object[]> files = new LinkedList<>();
+		files.addAll(getFiles(null, EXAMPLE_PROGRAM_PATH_WORKING));
+		files.addAll(getFiles(ParserException.class, EXAMPLE_PROGRAM_PATH_PARSE_FAILING));
+		files.addAll(getFiles(SemanticAnalyserException.class, EXAMPLE_PROGRAM_PATH_TYPE_FAILING));
+		files.addAll(getFiles(RunException.class, EXAMPLE_PROGRAM_PATH_RUNTIME_FAILING));
+		return files;
 	}
 }
