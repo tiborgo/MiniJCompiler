f13345c4955e65d557fda22afd615f7af417a6fd
Tibor Goldschwendt <goldschwendt@cip.ifi.lmu.de>
2015-01-18 18:41:28 +0100

    command line flags for each step in the compilation pipeline.

--- src/main/java/minijava/Configuration.java
+++ src/main/java/minijava/Configuration.java
@@ -1,5 +1,6 @@
 package minijava;
 
+import org.apache.commons.io.FilenameUtils;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
@@ -12,44 +13,80 @@ public class Configuration {
 	@Argument(usage = "Input file", required = true)
 	public String inputFile;
 	
-	@Option(name = "--output", usage = "Output file")
-	public String outputFile = "CC.out";
+	@Option(name = "--output", aliases = {"-o"}, usage = "Output file")
+	public String outputFile;
 	
 	@Option(name = "--verbose", aliases = {"-v"}, usage = "Print additional information")
 	public boolean verbose;
 	
-	@Option(name = "--silent", aliases = {"-s"})
+	@Option(name = "--silent", aliases = {"-s"}, usage = "Print no output at all")
 	public boolean silent;
 	
-	@Option(name = "--print-source-code", aliases = {"-sc"}, usage = "Pretty print the input source code", depends = "--verbose")
-	public boolean printSourceCode;
+	@Option(name = "--debug", aliases = {"-d"}, usage = "Print information in case of exceptions")
+	public boolean debug;
 	
-	@Option(name = "--print-intermediate", aliases = {"-i"}, usage = "Pretty print the input source code", depends = "--verbose")
-	public boolean printIntermediate;
-	
-	@Option(name = "--print-canonicalized-intermediate", aliases = {"-ci"}, usage = "Pretty print the input source code", depends = "--verbose")
-	public boolean printCanonicalzedIntermediate;
-	
-	@Option(name = "--print-pre-assembly", aliases = {"-pa"}, usage = "Prints the assembly with temporiaries and unspecified frame size", depends = "--verbose")
-	public boolean printPreAssembly;
-	
-	@Option(name = "--print-assembly", aliases = {"-a"}, usage = "Prints the final assembly", depends = "--verbose")
-	public boolean printAssembly;
-	
-	@Option(name = "--print-control-flow-graphs", aliases = {"-cfg"}, usage = "Prints the control flow graph", depends = "--verbose")
-	public boolean printControlFlowGraphs;
-	
-	@Option(name = "--print-interference-graphs", aliases = {"-ig"}, usage = "Prints the interference graphs", depends = "--verbose")
-	public boolean printInterferenceGraphs;
-	
-	@Option(name = "--run-executable", aliases = {"-re"}, usage = "Runs the compiled executable")
+	@Option(name = "--run-executable", aliases = {"-e"}, usage = "Runs the compiled executable", depends = {"--assembler"})
 	public boolean runExecutable;
 	
-	@Option(name = "--print-pre-colored-graphs", aliases = {"-pg"},  depends = "--verbose")
+	// steps of the compilation pipeline
+	@Option(name = "--parse", aliases = {"-p"},
+			forbids = {"--semantic-analysis", "--translate", "--canonicalize", "--instruction-selection", "--register-allocation", "--code-emission", "--assembler"},
+			usage = "compile until parse step")
+	public boolean parse;
+	@Option(name = "--semantic-analysis", aliases = {"-sa"},
+			forbids = {"--parse", "--translate", "--canonicalize", "--instruction-selection", "--register-allocation", "--code-emission", "--assembler"},
+			usage = "compile until semantic analysis step")
+	public boolean semanticAnalysis;
+	@Option(name = "--translate", aliases = {"-t"},
+			forbids = {"--parse", "--semantic-analysis", "--canonicalize", "--instruction-selection", "--register-allocation", "--code-emission", "--assembler"},
+			usage = "compile until translate step")
+	public boolean translate;
+	@Option(name = "--canonicalize", aliases = {"-c"},
+			forbids = {"--parse", "--semantic-analysis", "--translate", "--instruction-selection", "--register-allocation", "--code-emission", "--assembler"},
+			usage = "compile until canonicalize step")
+	public boolean canonicalize;
+	@Option(name = "--instruction-selection", aliases = {"-se"},
+			forbids = {"--parse", "--semantic-analysis", "--translate", "--canonicalize", "--register-allocation", "--code-emission", "--assembler"},
+			usage = "compile until instruction selection step")
+	public boolean instructionSelection;
+	@Option(name = "--register-allocation", aliases = {"-re"},
+			forbids = {"--parse", "--semantic-analysis", "--translate", "--canonicalize", "--instruction-selection", "--code-emission", "--assembler"},
+			usage = "compile until register allocation step")
+	public boolean registerAllocation;
+	@Option(name = "--code-emission", aliases = {"-ce"},
+			forbids = {"--parse", "--semantic-analysis", "--translate", "--canonicalize", "--instruction-selection", "--register-allocation", "--assembler"},
+			usage = "compile until code emission step")
+	public boolean codeEmission;
+	@Option(name = "--assembler", aliases = {"-a"},
+			forbids = {"--parse", "--semantic-analysis", "--translate", "--canonicalize", "--instruction-selection", "--register-allocation", "--code-emission"},
+			usage = "compile until assembler and linker step")
+	public boolean assembler;
+	
+	@Option(name = "--print-source-code", aliases = {"-psc"}, usage = "Pretty print the input source code")
+	public boolean printSourceCode;
+	
+	@Option(name = "--print-intermediate", aliases = {"-pi"}, usage = "Pretty print the intermediate code")
+	public boolean printIntermediate;
+	
+	@Option(name = "--print-canonicalized-intermediate", aliases = {"-pci"}, usage = "Pretty print the canonicalized intermediate code")
+	public boolean printCanonicalzedIntermediate;
+	
+	@Option(name = "--print-pre-assembly", aliases = {"-ppa"}, usage = "Prints the assembly with temporiaries and unspecified frame size")
+	public boolean printPreAssembly;
+	
+	@Option(name = "--print-assembly", aliases = {"-pa"}, usage = "Prints the final assembly")
+	public boolean printAssembly;
+	
+	@Option(name = "--print-control-flow-graphs", aliases = {"-pcfg"}, usage = "Prints the control flow graph")
+	public boolean printControlFlowGraphs;
+	
+	@Option(name = "--print-interference-graphs", aliases = {"-pig"}, usage = "Prints the interference graphs")
+	public boolean printInterferenceGraphs;
+	
+	@Option(name = "--print-pre-colored-graphs", aliases = {"-ppg"}, usage = "Prints the pre colored graphs")
 	public boolean printPreColoredGraphs;
 	
-	@Option(name = "--debug", aliases = {"-d"})
-	public boolean debug;
+	
 	
 	public Configuration(String[] args) {
 		
@@ -70,6 +107,15 @@ public class Configuration {
 			catch(CmdLineException e) {
 				System.err.println(e.getMessage());
 				throw new IllegalArgumentException("Failed parsing arguments", e);
+			}
+			
+			if (outputFile == null) {
+				if (codeEmission) {
+					outputFile = FilenameUtils.getBaseName(inputFile) + ".s";
+				}
+				else {
+					outputFile = FilenameUtils.getBaseName(inputFile) + ".out";
+				}
 			}
 			
 			Logger.setVerbose(verbose);
