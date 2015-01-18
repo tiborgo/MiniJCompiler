bbbc2834f3bab0e184737c76c37a3db51d768a6b
Tibor Goldschwendt <goldschwendt@cip.ifi.lmu.de>
2015-01-18 17:33:07 +0100

    A bit of cleanup

--- src/main/java/minijava/MiniJavaCompiler.java
+++ src/main/java/minijava/MiniJavaCompiler.java
@@ -32,9 +32,6 @@ import minijava.translate.layout.Fragment;
 import minijava.translate.layout.FragmentProc;
 import minijava.translate.layout.Label;
 import minijava.translate.tree.TreeStm;
-import minijava.util.SimpleGraph;
-
-//import java.nio.file.Files;
 
 public class MiniJavaCompiler {
 	public static final Path RUNTIME_DIRECTORY = Paths.get("src/main/resources/minijava/runtime");
@@ -43,121 +40,15 @@ public class MiniJavaCompiler {
 
 	public MiniJavaCompiler(MachineSpecifics machineSpecifics) {
 		this.machineSpecifics = machineSpecifics;
-	}
-
-	// Compiler pipeline
-
-	
-
-	
-
-	
-
-	
-
-	/*private List<SimpleGraph<Temp>> generateInterferenceGraphs(List<SimpleGraph<Assem>> controlFlowGraphs) throws CompilerException {
-
-		try {
-			List<SimpleGraph<Temp>> interferenceGraphs = new LinkedList<>();
-			for (SimpleGraph<Assem> controlFlowGraph : controlFlowGraphs) {
-				Map<Assem, LivenessSetsBuilder.InOut> inOut = LivenessSetsBuilder.build(controlFlowGraph);
-
-
-				Iterator<Assem> iter = inOut.keySet().iterator();
-				StringBuilder inOutStringBuilder = new StringBuilder();
-				inOutStringBuilder.append("[" + System.lineSeparator());
-				while (iter.hasNext()) {
-					Assem next = iter.next();
-					inOutStringBuilder
-						.append("\t   in: ")
-						.append(inOut.get(next).in)
-						.append(System.lineSeparator())
-						.append("\t")
-						.append(next)
-						.append(System.lineSeparator())
-						.append("\t   out:")
-						.append(inOut.get(next).out);
-					inOutStringBuilder
-						.append(System.lineSeparator())
-						.append(System.lineSeparator());
-				}
-				inOutStringBuilder.append("]");
-				System.out.println(inOutStringBuilder);
-
-				SimpleGraph<Temp> interferenceGraph = InterferenceGraphBuilder.build(controlFlowGraph, inOut);
-				interferenceGraphs.add(interferenceGraph);
-			}
-
-			String graphOutput = null;
-			if (printInterferenceGraphs) {
-				graphOutput = simpleGraphsToString(interferenceGraphs);
-			}
-
-			printVerbose("Successfully generated interference graphs", graphOutput);
-
-			return interferenceGraphs;
+		
+		String osName = System.getProperty("os.name").toLowerCase();
+		// TODO: better solution?
+		if (osName.contains("mac")) {
+			Label.leadingUnderscore = true;
 		}
-		catch (Exception e) {
-			// TODO: proper exception
-			throw new CompilerException("Failed to generate interference graphs", e);
+		else {
+			Label.leadingUnderscore = false;
 		}
-	}*/
-
-	private <T> String simpleGraphsToString (List<SimpleGraph<T>> graphs) throws CompilerException {
-
-		StringBuilder graphOutput = new StringBuilder();
-
-		for (SimpleGraph<T> graph : graphs) {
-			String dotCode = graph.getDot();
-
-			graphOutput.append(System.lineSeparator());
-			graphOutput.append(graph.getName() + System.lineSeparator());
-			graphOutput.append(System.lineSeparator());
-
-			graphOutput.append(dotCode);
-
-			/*try {
-				ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", "graph-easy --as boxart");
-				processBuilder.environment().put("PATH", "/usr/local/bin:" + processBuilder.environment().get("PATH"));
-				Process graphEasyCall = processBuilder.start();
-				OutputStream stdin = graphEasyCall.getOutputStream();
-				stdin.write(dotCode.getBytes());
-				stdin.close();
-
-				graphEasyCall.waitFor();
-
-				InputStream stdout = graphEasyCall.getInputStream();
-				BufferedReader bufferedStdout = new BufferedReader(new InputStreamReader(stdout));
-				String line;
-				while ((line = bufferedStdout.readLine()) != null) {
-					graphOutput.append(line + System.lineSeparator());
-				}
-				bufferedStdout.close();
-				stdout.close();
-
-				if (graphEasyCall.exitValue() != 0) {
-
-					StringBuilder errOutput = new StringBuilder();
-					InputStream stderr = graphEasyCall.getErrorStream();
-					BufferedReader bufferedStderr = new BufferedReader(new InputStreamReader(stderr));
-					while ((line = bufferedStderr.readLine()) != null) {
-						errOutput.append(line + System.lineSeparator());
-					}
-					bufferedStderr.close();
-					stderr.close();
-
-					throw new CompilerException("Failed to create print of control flow graph: " + errOutput.toString());
-				}
-			}
-			catch (IOException e) {
-				throw new CompilerException("Failed to transfer dot code to graph-easy", e);
-			}
-			catch (InterruptedException e) {
-				throw new CompilerException("Failed to invoke graph-easy", e);
-			}*/
-		}
-
-		return graphOutput.toString();
 	}
 	
 	public void compile(Configuration config) throws CompilerException {
@@ -278,16 +169,6 @@ public class MiniJavaCompiler {
 			System.exit(-1);
 			return;
 		}
-
-
-		String osName = System.getProperty("os.name").toLowerCase();
-		// TODO: better solution?
-		if (osName.contains("mac")) {
-			Label.leadingUnderscore = true;
-		}
-		else {
-			Label.leadingUnderscore = false;
-		}
 		
 		MiniJavaCompiler compiler = new MiniJavaCompiler(new I386MachineSpecifics());
 		
@@ -311,8 +192,8 @@ public class MiniJavaCompiler {
 		if (config.runExecutable) {
 			try {
 				compiler.runExecutable(config, 0);
-			} catch (RunException e) {
-				// TODO Auto-generated catch block
+			}
+			catch (RunException e) {
 				e.printStackTrace();
 				System.exit(-1);
 			}
