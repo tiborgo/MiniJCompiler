b9cf2099e2870c4be22ea7d86158d3058c0b19a9
Michael Seifert <mseifert@error-reports.org>
2015-01-19 17:19:27 +0100

    Added test case for switching two array elements.

---
+++ src/test/resources/minijava-examples/working/SwitchArrayElements.java
@@ -0,0 +1,21 @@
+class SwitchArrayElements {
+	public static void main(String[] a) {
+		System.out.println(new Switcher().Switch());
+	}
+}
+
+class Switcher {
+
+	public int Switch() {
+		int tmp;
+		int[] numbers;
+		numbers = new int[2];
+		numbers[0] = 88;
+		numbers[1] = 99;
+
+		tmp = numbers[0];
+		numbers[0] = numbers[1];
+		numbers[1] = tmp;
+		return 0;
+	}
+}
