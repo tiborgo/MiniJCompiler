9f1941acdbe794f74bedaa6fa138c8936bab2ee5
Tibor Goldschwendt <goldschwendt@cip.ifi.lmu.de>
2015-05-17 00:41:53 -0700

    Added license headers

--- src/main/java/minij/flowanalysis/ControlFlowGraphBuilder.java
+++ src/main/java/minij/flowanalysis/ControlFlowGraphBuilder.java
@@ -1,3 +1,24 @@
+/*
+ * MiniJCompiler: Compiler for a subset of the Java(R) language
+ *
+ * (C) Copyright 2014-2015 Tibor Goldschwendt <goldschwendt@cip.ifi.lmu.de>,
+ * Michael Seifert <mseifert@error-reports.org>
+ *
+ * This file is part of MiniJCompiler.
+ *
+ * MiniJCompiler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MiniJCompiler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MiniJCompiler.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package minij.flowanalysis;
 
 import java.util.HashMap;
